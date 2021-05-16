ce811c7ba3fe8d2574c86782056440b6a17042dc
Cerebreo <nellirism@outlook.com>
2021-05-16 14:54:08 -0700

    Accessing modules

---
+++ app.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+// var message = 'Hello Node';
+
+// var sum = 5 + 3;
+
+// console.log(message);
+// console.log(sum);
+
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+
+// var profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+// console.log(profileDataArgs);
+
+// // notice the lack of parentheses around the 'profileDataArr' parameter?
+// const printProfileData = (profileDataArr) => {
+//     // This ...
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('=====================');
+    
+//     // Is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+
+// printProfileData(profileDataArgs)
+// };
+// using function expression syntax
+// const addNums = function(numOne, numTwo) {
+//    return numOne + numTwo;
+// };
+
+// or using just one line (implicit return or just one action)
+// const addNums = (numOne, numTwo) => numOne + numTwo;
+// const sum = addNums(5, 3); // sum would be 8
+
+// or using multiple actions
+
+// const addNums = (numOne, numTwo) => {
+//     console.log(numOne, numTwo);
+//     return numOne + numTwo;
+// };
+
+// Using new arrow function syntax
+// const addNums = (numOne, numTwo) => {
+//      return numOne + numTwo;
+
+// const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+
+// multi-line input - recoded the above arrow funx
---
+++ index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const math = require('./math');
+
+console.log(math.add(2, 3));
+
+// const textMessage = fs.readFile("./message.txt", "utf8", (err, data) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(data);
+// });
