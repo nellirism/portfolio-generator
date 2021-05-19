9898ccc9933fcb99a5f8cf2f9caf118152b428b3
Cerebreo <nellirism@outlook.com>
2021-05-18 20:53:42 -0700

    Modifying html template to handle more data

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'about',
-            message: 'Provide some infirmation about yourself:',
+            message: 'Provide some information about yourself:',
             when: ({ confirmAbout }) => confirmAbout
         },
     ]);
@@ -212,13 +212,13 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    // console.log(portfolioData);
     // will be uncommented in lesson 4
-    // const pageHTML = generatePage(portfolioData);
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+    const pageHTML = generatePage(portfolioData);
+      fs.writeFile('./index.html', pageHTML, err => {
+         if (err) throw new Error(err);
+         console.log('Page created! Check out index.html in this directory to see it!');
+      });
   });
 
 // console.log(inquirer);
